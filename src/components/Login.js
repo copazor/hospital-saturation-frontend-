e2b7cc3aecee6eef01544ce841ca3d9660c61ce5
Christopher <christopher@MacBook-Air-de-Christopher.local>
2025-09-04 18:43:04 -0400

    Update login API URL to deployed backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -27,7 +27,7 @@ function Login() {
     formData.append('password', password);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/token', {
+      const response = await fetch('https://hospital-saturation-backend.onrender.com/token', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
